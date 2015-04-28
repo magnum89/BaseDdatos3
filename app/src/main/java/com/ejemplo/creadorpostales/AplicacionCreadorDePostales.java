379c3b146a444708c323cb6358ace0a05d91509c
magnum89 <rojasdrm03@hotmail.com>
2015-04-27 21:01:08 -0500

    Leyendo desde almacenamiento interno

--- app/src/main/java/com/ejemplo/creadorpostales/AplicacionCreadorDePostales.java
+++ app/src/main/java/com/ejemplo/creadorpostales/AplicacionCreadorDePostales.java
@@ -8,8 +8,8 @@ public class AplicacionCreadorDePostales extends android.app.Application {
     public void onCreate() {
         super.onCreate();
 
-        FileUtilities.saveAssetImage(this, "perro.jpg");
-        FileUtilities.saveAssetImage(this, "gato.jpg");
+        FileUtilities.saveAssetImage(this, "perro.jpg");//este es el contexto con  el cual estamos copiando
+        FileUtilities.saveAssetImage(this, "gato.jpg");//estas imagenes assets
         FileUtilities.saveAssetImage(this, "cachorro.jpg");
     }
 }
--- app/src/main/java/com/ejemplo/creadorpostales/utils/FileUtilities.java
+++ app/src/main/java/com/ejemplo/creadorpostales/utils/FileUtilities.java
@@ -1,6 +1,7 @@
 package com.ejemplo.creadorpostales.utils;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
@@ -9,11 +10,26 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 
 public class FileUtilities {
 
     public static void saveAssetImage(Context context, String assetName) {
+
+        //manejador de archivos
+        File directorioArchivos = context.getFilesDir();
+        File archivoAEscribir = new File(directorioArchivos, assetName);//nos va a dar un archivo dentro de la ruta directoriArchivos
+        //y el nombre de este archivo se encuentra en la variable de assetName, tambien se escribe informacion en el almacenamiento interno
+
+        //tener acceso a los archivos asserts para poder leerlos
+        AssetManager manejadorDeAssets = context.getAssets();
+        //crear una corriente de archivos o fileStreem para las entradas
+        //son una manera simplificada para leer o escribir archivos en el sistema de archivo
+        InputStream is = manejadorDeAssets.open(assetName);
+        //para indicar donde queremos copiar este archivo que acabamos de leer
+
 
     }
 
